f1c49ed84ab7769f12e2096722b593b06755bd03
Anik Chand <anikchand461@gmail.com>
2026-05-30 20:49:49 +0530

    initial

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"backend/routes"
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	r := gin.Default()
+
 	r.Use(cors.Default())
+
 	routes.UserRoutes(r)
-	r.Run(":8080")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run(":" + port)
 }
--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/users"
+const API_URL = "https://YOUR-RENDER-URL.onrender.com/users"
 
 function showMessage(text, color) {
 
